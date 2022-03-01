f2143c7b9a39339756d943e67df4ee5f7622ada5
Andrei Constantinescu <andreiec112@gmail.com>
2022-03-02 00:07:29 +0200

    Redesigned login form using formik, added validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,73 +1,129 @@
-import { Flex, Text, Button, Center, Box, FormControl, FormLabel, Input, Link, Alert, AlertIcon, CloseButton } from "@chakra-ui/react";
+import { Flex, Text, Button, Center, Box, FormControl, FormLabel, Input, Link, Alert, AlertIcon, CloseButton, FormErrorMessage, } from "@chakra-ui/react";
 import { useRef, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from 'react-router-dom'
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import { authActions } from "../store";
-import apiClient from '../http-common';
+import apiClient from "../http-common";
+import { Form, Formik, Field, ErrorMessage } from "formik";
+import * as Yup from 'yup';
+
+
+const initialValues = {
+    email: "",
+    password: "",
+}
+
+const validationSchema = Yup.object({
+    email: Yup.string().email('Email invalid').required('Obligatoriu'),
+    password: Yup.string().required('Obligatoriu')
+})
 
 
 function Login() {
-    const emailInputRef = useRef();
-    const passwordInputRef = useRef();
-
     const navigate = useNavigate();
     const authDispatch = useDispatch();
 
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-
-    function submitHandler() {
-        const enteredEmail = emailInputRef.current.value;
-        const enteredPassword = passwordInputRef.current.value;
-
+    const submitHandler = (values, submitProps) => {
         setIsLoading(true);
 
-        apiClient.post('auth/token/', {
-            email: enteredEmail,
-            password: enteredPassword,
+        apiClient.post("auth/token/", {
+            email: values.email,
+            password: values.password,
         }).then((response) => {
             setIsLoading(false);
             setError(null);
-            authDispatch(authActions.login(response.data['access']));
-            navigate('/');
-        }).catch(err => {
+            authDispatch(authActions.login(response.data["access"]));
+            navigate("/");
+        }).catch((err) => {
             setIsLoading(false);
             setError(err);
         });
     };
 
     return (
-        <Center h='100vh' bg={{base: 'brand.white', md: 'brand.bg'}}>
-            <Flex flexDir="column" alignItems='center' mb='35px'>
-                <Text fontWeight='bold' fontSize="3xl" color='brand.text_title'>Bun venit!</Text>
-                <Text fontSize="sm" color='brand.text_body'>Autentifică-te pentru a continua.</Text>
+        <Center h="100vh" bg={{ base: "brand.white", md: "brand.bg" }}>
+            <Flex flexDir="column" alignItems="center" mb="35px">
+                <Text fontWeight="bold" fontSize="3xl" color="brand.text_title">
+                    Bun venit!
+                </Text>
+                <Text fontSize="sm" color="brand.text_body">
+                    Autentifică-te pentru a continua.
+                </Text>
 
-                <Box bg='brand.white' borderRadius='10px' mt='30px' >
-                    <Box m={{base: '0px', md: '40px'}} p={{base: '40px', md: '0px'}} w={{base: '100vw', md: '340px' }}>
-                        {error && <Alert status='error' my='25px'><AlertIcon />Email-ul sau parola este greșită.<CloseButton position='absolute' right='8px' top='8px' onClick={() => setError(false)}/></Alert>}
+                <Box bg="brand.white" borderRadius="10px" mt="30px">
+                    <Box
+                        m={{ base: "0px", md: "40px" }}
+                        p={{ base: "40px", md: "0px" }}
+                        w={{ base: "100vw", md: "340px" }}
+                    >
+                        {error && (
+                            <Alert status="error" my="25px">
+                                <AlertIcon />
+                                Email-ul sau parola greșită.
+                                <CloseButton
+                                    position="absolute"
+                                    right="8px"
+                                    top="8px"
+                                    onClick={() => setError(false)}
+                                />
+                            </Alert>
+                        )}
+                        <Formik
+                            initialValues={initialValues}
+                            validationSchema={validationSchema}
+                            onSubmit={submitHandler}
+                        >
+                            <Form>
+                                <Field name='email'>
+                                    {({ field, form }) => (
+                                        <FormControl isInvalid={form.errors.email && form.touched.email}>
+                                            <FormLabel fontWeight="600" htmlFor="email">E-mail</FormLabel>
+                                            <Input {...field} id="email" />
+                                            <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                                        </FormControl>
+                                    )}
+                                </Field>
 
-                        <form>
-                            <FormControl>
-                                <FormLabel fontWeight='600' htmlFor='email'>E-mail</FormLabel>
-                                <Input id='email' type='email' required ref={emailInputRef} />
-                                <FormLabel fontWeight='600' htmlFor='password' mt='10px'>Parolă</FormLabel>
-                                <Input id='password' type='password' required ref={passwordInputRef} />
-                            </FormControl>
+                                <Field name='password'>
+                                    {({ field, form }) => (
+                                        <FormControl mt='10px' isInvalid={form.errors.password && form.touched.password}>
+                                            <FormLabel fontWeight="600" htmlFor="password">Parolă</FormLabel>
+                                            <Input {...field} id="password" type="password" />
+                                            <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+                                        </FormControl>
+                                    )}
+                                </Field>
 
-                            <Button onClick={submitHandler} isLoading={isLoading} w='full' mt='25px' bg='brand.main_blue' _hover={{bg:'brand.blue_light'}} color='brand.white' fontWeight='400'>Autentifică-te</Button>
-                        </form>
+                                <Button
+                                    isLoading={isLoading}
+                                    w="full"
+                                    mt="25px"
+                                    bg="brand.main_blue"
+                                    _hover={{ bg: "brand.blue_light" }}
+                                    color="brand.white"
+                                    fontWeight="400"
+                                    type="submit"
+                                >
+                                    Autentifică-te
+                                </Button>
+                            </Form>
+                        </Formik>
 
-                        <Box mt='10px'>
-                            <Link href='#' color='brand.main_blue'>Parolă uitată?</Link>
+
+                        <Box mt="10px">
+                            <Link href="#" color="brand.main_blue">
+                                Parolă uitată?
+                            </Link>
                         </Box>
                     </Box>
                 </Box>
-
             </Flex>
         </Center>
-    )
+    );
 }
 
 export default Login;
